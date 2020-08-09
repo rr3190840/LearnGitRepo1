609373be7237411a03512fd49615619cffb6d396
Ravi Ranjan <rr3190840@gmail.com>
2020-08-09 10:40:34 +0530

    Initial commit

---
+++ HelloWorld.py
@@ -0,0 +1,2 @@
+print('This world is full of mistry')
+print('The world is open for open minded people to give whatever they want')
---
+++ for_loop.py
@@ -0,0 +1,2 @@
+for i in range (10):
+	print('The text is not going to add the glorry')
